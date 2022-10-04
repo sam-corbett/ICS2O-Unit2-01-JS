5d1f2955275d6368da4dbe07f2eca0f3c05d43e8
sam-corbett <113055935+sam-corbett@users.noreply.github.com>
2022-10-04 14:22:23 -0400

    javascript code added

---
+++ js/style.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Sam Corbett All rights reserved
+//
+// Created by: Sam Corbett
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+ function myButtonClicked() {
+    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  }
